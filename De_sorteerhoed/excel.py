00c88e89de77122ca7ddacaabe128b3adaa4909d
tiemob <70651774+tiemob@users.noreply.github.com>
2021-11-03 11:04:23 +0100

    Update excel.py

--- De_sorteerhoed/excel.py
+++ De_sorteerhoed/excel.py
@@ -1,32 +1,30 @@
-
- 
 import openpyxl
  
-# Give the location of the file
+# Locatie Vragen
 path = "Vragen.xlsx"
  
-# To open the workbook
-# workbook object is created
+# Werkblad openen
+# Werkblad creëeren
 wb_obj = openpyxl.load_workbook(path)
  
-# Get workbook active sheet object
-# from the active attribute
+# pakt het goeie sheet
 sheet_obj = wb_obj.active
- 
-# Cell objects also have a row, column,
-# and coordinate attributes that provide
-# location information for the cell.
- 
-# Note: The first row or
-# column integer is 1, not 0.
- 
-# Cell object is created by using
-# sheet object's cell() method.
-cell_obj = sheet_obj.cell(row = 2, column = 2)
- 
-# Print value of cell object
-# using the value attribute
-print(cell_obj.value)
 
+#Start van de Automatische picker
+Vragen = int(0)
+Antwoord = int (0)
+Antwoord = int (0)
+#18 vragen dus kleiner dan 18 
+while Vragen < 18:
+    Rij = 3
+    Kolom = 2
+    Vragen = Vragen + 1
+    Antwoord = Antwoord +1
+# er zijn 5 rijen die we gebruiken en dat begint op 2 dus kleiner dan 6 
+    while Antwoord < 6:
+        Question = sheet_obj.cell(row=Rij, column=Kolom)
+        Antwoord = Antwoord +1
+        Kolom = Kolom+1
+        print(Question.value)
 
 
